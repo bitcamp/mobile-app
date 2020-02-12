ea19d45c53e15c0d8cca1f4e7c08445439b40396
Zachary Breit <zbreit18@gmail.com>
2020-02-11 22:15:32 -0500

    Fixed import statement spacing

--- src/events/EventsManager.js
+++ src/events/EventsManager.js
@@ -1,6 +1,6 @@
 // Use this class to interact with all of the events, never modify the state directly
 
-import { AsyncStorage} from "react-native";
+import { AsyncStorage } from "react-native";
 import Toast from "react-native-tiny-toast";
 // import firebase from "firebase";
 import moment from "moment";
